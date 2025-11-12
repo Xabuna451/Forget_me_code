2105402e04cd301bc19fef18bb99d34dc39fe4a6
1227ycygif <1227ycy@kyonggi.ac.kr>
2025-11-12 14:45:34 +0900

    Update GameManager.cs

--- 02. Scripts/GameManager.cs
+++ 02. Scripts/GameManager.cs
@@ -47,7 +47,7 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha4))
+        if (Input.GetKeyDown(KeyCode.Alpha7))
         {
             TestSaveAllData();
         }
