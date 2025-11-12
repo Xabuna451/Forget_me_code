8008ee6245145199e5f9879eca10b12789eaabb0
518kjy <518kjy@gmail.com>
2025-11-12 14:54:29 +0900

    Update GameManager.cs
    local to git.dev (KJY)

--- 02. Scripts/GameManager.cs
+++ 02. Scripts/GameManager.cs
@@ -47,7 +47,7 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha7))
+        if (Input.GetKeyDown(KeyCode.Alpha9))
         {
             TestSaveAllData();
         }
